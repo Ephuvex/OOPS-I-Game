405a2773b4ab8bdc11c7d5f13fa7c3e8ff76ebb6
Andrew Kaiser <andrew@paradigmstopractices.com>
2017-12-14 15:29:34 -0600

    Integrated some of Quinn's changes in
    Signed-off-by: Andrew Kaiser <andrew@paradigmstopractices.com>

--- src/PFigureList.java
+++ src/PFigureList.java
@@ -14,26 +14,35 @@ public class PFigureList
    public final int MAX_SIZE = 10;
    private PFigure list[] = new PFigure[MAX_SIZE];
    private int numFigs = 0;
-   
-   
+
+
    private int find ( PFigure fig )
    {
-       for (int i = numFigs -1; i > -1; i--)
-           if (list[i].equals(fig))
-               return i;
-       return -1;
+      for ( int i = numFigs - 1; i > -1; i-- )
+         if ( list[i].equals(fig) )
+            return i;
+      return -1;
    }
-   
+
    public boolean delete ( PFigure fig )
    {
-       int index = find (fig);
-       if ( index > -1)
-       {
-           for (int i = index; i < numFigs -1; i++)
-                  list[i] = list[i + 1];
-              numFigs--;
-              return true;
-       }
-       return false;       
+      int index = find(fig);
+      if ( index > -1 )
+      {
+         for ( int i = index; i < numFigs - 1; i++ )
+            list[i] = list[i + 1];
+         numFigs--;
+         return true;
+      }
+      return false;
+   }
+
+   public void add( PFigure fig )
+   {
+      if ( numFigs < this.MAX_SIZE )
+      {
+         list[numFigs - 1] = fig;
+         numFigs++;
+      }
    }
 }
