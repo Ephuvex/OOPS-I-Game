a0eb6f282846c3ad5ce9492a035bf2f449c00d18
Trollczar <34317902+Trollczar@users.noreply.github.com>
2017-12-18 18:56:54 -0600

    Add files via upload

--- src/colliders/nonrailcolliders/Ball.java
+++ src/colliders/nonrailcolliders/Ball.java
@@ -5,47 +5,78 @@ import src.colliders.railcolliders.RailCollider;
 import java.awt.*;
 
 /**
- @author dericksonb */
+ This class creates the ball BrickBreaker Game. This class also handles the  
+ the move, draw and collides function for the ball. 
+ @author dericksonb 
+*/
 public class Ball extends NonRailCollider
 {
     private final static int RADIUS = 5;
 
-    public Ball( int initX, int initY, int initXVel, int initYVel, Panel p )
+    /**
+     THis is the constructor of the ball. It takes in the starting position
+     based on where the paddle is or where a duplicate brick is. 
+     @param initX The Starting X position of the ball
+     @param initY The Starting Y position of the ball
+     @param initXVel The Starting X velocity of the ball
+     @param initYVel The Starting Y velocity of the ball
+     @param p the panel of the BreakerApplet
+     */
+    
+    public Ball( int initX, int initY, int initXVel, int initYVel, 
+            Color initColor, Panel p )
     {
         super(initX, initY, RADIUS * 2, RADIUS * 2, 1, p);
         xVel = initXVel;
         yVel = initYVel;
+        
     }
 
+    /**
+    This method is used if the ball bounces off the paddle of the game. If does
+    it then reverses the X velocity. 
+    */
     @SuppressWarnings("WeakerAccess")
     public void bounceX()
     {
         yVel = -yVel;
     }
 
+    /**
+    This method is used if the ball bounces off the paddle of the game. If does
+    * it then reverses the Y velocity. 
+    */
     @SuppressWarnings("WeakerAccess")
     public void bounceY()
     {
         xVel = -xVel;
     }
 
+    
+    /**
+    This method overwrites the move function of the parent class to move 
+    the missile. If the ball hits the wall of the applet then it reverses the 
+    x velocity and if it hits the ceiling of the Applet then it reverses the 
+    y velocity. 
+     */
     @Override
     public void move()
     {
         if ( xVel < 0 && x - RADIUS <= 0 ||
                 xVel > 0 && x + RADIUS >= panel.getSize().width )
             xVel = -xVel;
-
-
         if ( yVel < 0 && y - RADIUS <= 0 ||
                 yVel > 0 && y + RADIUS >= panel.getSize().height )
             yVel = -yVel;
-
 
         x = x + xVel;
         y = y + yVel;
     }
 
+    /**
+    This method overwrites the draw function of the parent class to draw 
+    the ball on the panel. 
+     */
     @Override
     public void draw()
     {
@@ -53,6 +84,10 @@ public class Ball extends NonRailCollider
         g.fillOval(x, y, 10, 10);
     }
 
+    /**
+    This method overwrites the isOutOfBounds of the parent class. Checks of the
+    ball is hitting the wall or ceiling of the panel. 
+    */
     @Override
     public boolean isOutOfBounds()
     {
@@ -65,6 +100,11 @@ public class Ball extends NonRailCollider
     {
     }
 
+    /**
+    This method overwrites the didCollideWith function of the parent class to
+    see if the ball collided with anything and if it was a brick then it 
+    destroys the brick. 
+     */
     @Override
     public void didCollideWith( RailCollider railCollider )
     {
@@ -74,11 +114,13 @@ public class Ball extends NonRailCollider
         if ( railCollider.didHitTopBottom(this) )
             this.bounceY();
     }
-
+    /**
+    This method overwrites the kill function of the parent class to kill 
+    the ball on the panel if its required. 
+     */
     @Override
     public void kill()
     {
-        //noinspection UnnecessaryReturnStatement
         return;
     }
 }
--- src/colliders/nonrailcolliders/Missile.java
+++ src/colliders/nonrailcolliders/Missile.java
@@ -1,8 +1,4 @@
-package src.colliders.nonrailcolliders;/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package src.colliders.nonrailcolliders;
 
 
 import src.colliders.railcolliders.RailCollider;
@@ -10,13 +6,29 @@ import src.colliders.railcolliders.RailCollider;
 import java.awt.*;
 
 /**
- @author dericksonb */
+ This Class creates the Missile for BrickBreaker game. This uses the parent 
+ class NonRailCollider and fires a missile when the player hits the space 
+ button. 
+ @author dericksonb 
+ */
 public class Missile extends NonRailCollider
 {
     public static int angle = 90;
     public static int counter = 10;
+    
     public static boolean isMissileInPlay = false;
 
+    /**
+     THis is the constructor of the missile. It takes in the starting position
+     based on where the directional arrow of the paddle is. Also Sets the 
+     missile off at a predetermined speed of the missile and sends it on its
+     way in the direction the player chooses. 
+     @param initX The Starting X position of the missile
+     @param initY The Starting Y position of the missile
+     @param initXVel The Starting X velocity of the missile
+     @param initYVel The Starting Y velocity of the missile
+     @param p the panel of the BreakerApplet
+     */
     public Missile( int initX, int initY, int initXVel, int initYVel, Panel p )
     {
         super(initX, initY, 10, 10, 0, p);
@@ -24,6 +36,10 @@ public class Missile extends NonRailCollider
         yVel = initYVel;
     }
 
+    /**
+    This method overwrites the move function of the parent class to move 
+    the missile. 
+     */
     @Override
     public void move()
     {
@@ -31,6 +47,10 @@ public class Missile extends NonRailCollider
         y += yVel;
     }
 
+    /**
+    This method overwrites the draw function of the parent class to draw 
+    the missile on the panel. 
+     */
     @Override
     public void draw()
     {
@@ -39,6 +59,11 @@ public class Missile extends NonRailCollider
         g.fillOval(x, y, 10, 10);
     }
 
+    
+    /**
+    This method overwrites the didCollideWith function of the parent class to
+    see if the missile collided with anything and if it does it kills itself. 
+     */
     @Override
     public void didCollideWith( RailCollider collider )
     {
@@ -48,6 +73,11 @@ public class Missile extends NonRailCollider
         }
     }
 
+    
+    /**
+    This method overwrites the kill function of the parent class to destroy the
+    missile if it collided with a brick. 
+     */
     @Override
     public void kill()
     {
